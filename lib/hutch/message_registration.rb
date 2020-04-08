62478004e8f91cabd51a46e30ce80817a4f66277
Jan Mohoric <jan@vertical-life.info>
2020-04-08 10:56:48 +0200

    update message registration object

--- lib/hutch/message_registration.rb
+++ lib/hutch/message_registration.rb
@@ -1,10 +1,25 @@
 module Hutch
   module MessageRegistration
 
-    def register queue
-      p "***A"
-      Hutch.register_message(queue)
+    def self.included(base)
+      base.extend(ClassMethods)
     end
+
+    module ClassMethods
+
+      def register
+        p self
+        p "***"
+        Hutch.register_message(self)
+      end
+
+      def set_queue name
+        @queue_name = name
+      end
+
+      def get_queue
+        @queue_name
+      end
 
   end
 end
