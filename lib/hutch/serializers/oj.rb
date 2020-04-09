d4f8c1093aeed5d9b1c96f9aa1bafdcc08fb961a
Jan Mohoric <jan@vertical-life.info>
2020-04-09 13:41:28 +0200

    change oj mode to rails

--- lib/hutch/serializers/oj.rb
+++ lib/hutch/serializers/oj.rb
@@ -6,15 +6,15 @@ module Hutch
     class Oj
 
       def self.encode(payload)
-        ::Oj.dump(payload, mode: :custom)
+        ::Oj.dump(payload, mode: :rails)
       end
 
       def self.decode(payload)
-        ::Oj.load(payload, mode: :custom)
+        ::Oj.load(payload, mode: :rails)
       end
 
       def self.decode(payload, object_class)
-        ::Oj.load(payload, mode: :custom, object_class: object_class)
+        ::Oj.load(payload, mode: :rails, object_class: object_class)
       end
 
       def self.binary? ; false ; end
